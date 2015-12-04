fe23bc49b542ac3d006624f47b9cb2420c9eaea4
ouskah <ouskah.gn@gmail.com>
2015-12-04 01:18:18 +0000

    Ajout choix : catégorie du livre

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,14 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @categories = Category.all
   end
 
   def create
-    Book.create title: params[:title]
+    Book.create title: params[:title], category_id: params[:category_id]
     redirect_to "/books"
   end
-
+  
   def show
     @book = Book.find(params[:id])
   end
