a7992672fc4a18815eccd368cf0eb0c40296bcbb
ouskah <ouskah.gn@gmail.com>
2015-12-04 15:02:06 +0000

    Ajout des scopes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,17 @@
 class Book < ActiveRecord::Base
   belongs_to :category
   
-  validates :title, presence: {
+    validates :title, presence: {
       message: "Le titre doit être renseigné"
-  }
+    }
     validates :title, uniqueness: {
         message: "Ce titre est déjà renseigné"
     }
+    
+    scope :french, -> {where(category_id: 1)}
+    
+    scope :english, -> {where(category_id: 2)}
+    
+    
+    
 end
