8e85802b09be26e004c7d2da3ba902059f6ab66d
ouskah <ouskah.gn@gmail.com>
2015-12-04 13:41:27 +0000

    Ajout des validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,10 @@
 class Book < ActiveRecord::Base
   belongs_to :category
+  
+  validates :title, presence: {
+      message: "Le titre doit être renseigné"
+  }
+    validates :title, uniqueness: {
+        message: "Ce titre est déjà renseigné"
+    }
 end
